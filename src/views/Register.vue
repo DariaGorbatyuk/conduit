15183e8569857a907810d0eb5a9d2c5183f13fbc
Daria <gorbatyukdaria@gmail.com>
2022-07-21 23:13:30 +0300

    Add registration

---
+++ src/views/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="auth-page">
+    <div class="container-page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link to="{name: 'login}">Need an account?</router-link>
+          </p>
+          VALIDATION ERRORS
+          <form action="" @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input type="text" class="form-control form-control-lg" placeholder="Username">
+            </fieldset>
+            <fieldset class="form-group">
+              <input type="email" class="form-control form-control-lg" placeholder="Email">
+            </fieldset>
+            <fieldset class="form-group">
+              <input type="password" class="form-control form-control-lg" placeholder="Password">
+            </fieldset>
+            <button type="submit" class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "McvRegister",
+  setup() {
+    const onSubmit = () => {
+
+    }
+    return {
+      onSubmit
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
