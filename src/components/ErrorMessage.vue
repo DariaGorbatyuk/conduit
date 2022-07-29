79b622bcae2c5e1ebe629de1d5e77b09c1aa247c
aggo <d.gorbatyuk@digitaljet.ru>
2022-07-29 11:12:12 +0300

    Добавляет error component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: false,
+    default: "Something went wrong"
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <mcv-loading  v-if="isLoading"></mcv-loading>
-    <div v-if="errors">{{ errors }}</div>
+    <mcv-loading v-if="isLoading"></mcv-loading>
+    <mcv-error-message v-if="errors"></mcv-error-message>
     <div v-if="feed">
       <div class="article-preview" v-for="(article, idx) in feed.articles" :key="idx">
         <div class="article-meta">
@@ -41,7 +41,9 @@ import McvPagination from "@/components/Pagination";
 import { LIMIT } from "@/helpers/vars";
 import { useRoute } from "vue-router/dist/vue-router";
 import { stringify, parseUrl } from "query-string";
-import McvLoading from "@/components/Loading"
+import McvLoading from "@/components/Loading";
+import McvErrorMessage from "@/components/ErrorMessage";
+
 const store = useStore();
 const route = useRoute();
 const props = defineProps({
